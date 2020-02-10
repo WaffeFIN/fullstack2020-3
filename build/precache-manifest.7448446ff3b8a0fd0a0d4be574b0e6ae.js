e9263280cf859d7620f88a5228d74512025b6615
Walter Grönholm <waltergronholm@gmail.com>
2020-02-10 18:07:54 +0200

    uibuild

---
+++ build/precache-manifest.7448446ff3b8a0fd0a0d4be574b0e6ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519403515ccda0d44a2814a4788535c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bf8223325e12a442398",
+    "url": "/static/css/main.cf333068.chunk.css"
+  },
+  {
+    "revision": "8514db5cee128984cd03",
+    "url": "/static/js/2.16d1eaee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.16d1eaee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5bf8223325e12a442398",
+    "url": "/static/js/main.ffd7208a.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
