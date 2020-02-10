1bbfead18763831ac2b44940da4868d342115cb6
Walter Grönholm <waltergronholm@gmail.com>
2020-02-10 17:36:14 +0200

    osa3a

---
+++ src/index.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const app = express()
+
+const morgan = require('morgan')
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
+app.use(morgan(
+    ':method :url :status :response-time ms :: :body'
+))
+
+app.use(express.json())
+
+let persons = [
+    {
+      id: 1,
+      name: "Pelle peloton",
+      number: "040-2131231",
+    },
+    {
+      id: 2,
+      name: "ASD peloton",
+      number: "040-1861237",
+    },
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has ${persons.length} entries!</p><p>${new Date(Date.now())}</p>`)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.post('/api/persons', (req, res) => {
+    if (!req.body.name) {
+      return res.status(400).json({ 
+        error: 'No name' 
+      })
+    }
+    if (!req.body.number) {
+      return res.status(400).json({ 
+        error: 'No number' 
+      })
+    }
+
+    const id = Math.floor(Math.random() * 16 * 1024 * 1024)
+    const person = { ...req.body, id: id }
+    if (persons.reduce((value, p) => value || person.name === p.name, false)) {
+      return res.status(400).json({ 
+        error: 'Person already exists' 
+      })
+    }
+    console.log(`Added ${person}`)
+    persons.push(person)
+
+    res.json(person)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.send()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
